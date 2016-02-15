a38a6837f274f2091916cba49f1a14a3ee6673c7
Lotte Ravn <lotteravn@lottes-mac-mini.local>
2016-02-15 13:49:28 +0100

    Initial Commit

---
+++ mail2Tests/mail2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  mail2Tests.swift
+//  mail2Tests
+//
+//  Created by Lotte Ravn on 15/02/16.
+//  Copyright © 2016 Lotte Ravn. All rights reserved.
+//
+
+import XCTest
+@testable import mail2
+
+class mail2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
